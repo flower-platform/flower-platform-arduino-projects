e0d3b056f6a83a63608b547400f9edfbb7265ec9
Claudiu Matei <claudiu@shmessenger.org>
2015-05-19 14:14:44 +0300

    Template optimizations

--- sample01/spec.d
+++ sample01/spec.d
@@ -1,2 +1,2 @@
 spec.o: \
- D:/data/java_work/eclipse_workspace/arduino2/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
+ F:/Flower/Workspaces/arduino/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
