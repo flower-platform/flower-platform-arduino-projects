b33a448cecbd47fa843e8540e2f7ac5e8509581f
Claudiu Matei <claudiu@shmessenger.org>
2015-09-30 14:26:04 +0300

    Clean up project "ir-test".

--- ir-test/.ino.cpp
+++ ir-test/.ino.cpp
@@ -1,16 +1,23 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-09-18 12:23:54
+//This file has been generated on 2015-09-30 12:40:59
 
 #include "Arduino.h"
-#include <IRCommand.h>
-#include <Ethernet.h>
+#include <avr/pgmspace.h>
+#include <Arduino.h>
+#include <Client.h>
 #include <FlowerPlatformArduinoRuntime.h>
-#include <SPI.h>
-#include <SdFat.h>
+#include <HardwareSerial.h>
 #include <HttpServer.h>
-#include <IRremote.h>
+#include <IRCommand.h>
+#include <IRLearner.h>
+#include <pins_arduino.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <WiFiNetworkAdapter.h>
+#include <WString.h>
 #include "CustomCode.h"
 void setup() ;
 void loop() ;
