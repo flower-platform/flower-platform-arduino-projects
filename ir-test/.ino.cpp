5697c17f12ed51227425e5d8d95695369303e01d
Claudiu Matei <claudiu@shmessenger.org>
2015-11-27 13:56:23 +0200

    Motor controller + robot project; template update for "hasCustomCode" flag; buffering optimizations

--- ir-test/.ino.cpp
+++ ir-test/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-10-20 18:43:38
+//This file has been generated on 2015-11-24 17:07:53
 
 #include "Arduino.h"
 #include <avr/pgmspace.h>
--- ir-test/ir-test.ino
+++ ir-test/ir-test.ino
@@ -33,7 +33,6 @@ protected:
 
 		if (strcmp_P(command, PSTR("")) == 0) {
 			urlFound = true;
-<<<<<<< HEAD
 			castedEvent->server->httpSuccess(&out, HttpServer::CONTENT_TYPE_HTML);
 			write_P(&out, PSTR("<!DOCTYPE html><html><head><meta charset=\"ISO-8859-1\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><script src=\"http:\/\/code.jquery.com\/jquery-1.11.2.min.js\"><\/script><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.4\/css\/bootstrap.min.css\"><script src"));
 			write_P(&out, PSTR("=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.4\/js\/bootstrap.min.js\"><\/script><link rel=\"stylesheet\" href=\"http:\/\/bootswatch.com\/paper\/bootstrap.min.css\"><link href='http:\/\/fonts.googleapis.com\/css?family=Roboto' rel='stylesheet' type='text\/css'><script>var url=\"\";var bindingManager={data:{},liste"));
@@ -46,19 +45,6 @@ protected:
 			write_P(&out, PSTR("tton><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('acOff')\"> AC OFF <\/button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('acOn24')\"> AC ON 24C <\/button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('acFanHigh')\"> acFanHigh <\/button><\/"));
 			write_P(&out, PSTR("div><\/div><\/div><\/div><div class=\"row\"><div class=\"col-md-12\"><span class=\"label label-default\"><span class=\"glyphicon glyphicon-dashboard\"><\/span> Free memory (SRAM): <span bindToProperty=\"FREE_MEM\"><\/span> bytes<\/span><\/div><\/div><\/div><\/body>"));
 			out.flush();
-=======
-			castedEvent->server->httpSuccess(HttpServer::CONTENT_TYPE_HTML);
-			castedEvent->client->print(F("<!DOCTYPE html><html><head><meta charset=\"ISO-8859-1\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><script src=\"http:\/\/code.jquery.com\/jquery-1.11.2.min.js\"><\/script><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.4\/css\/bootstrap.min.css\"><script src"));
-			castedEvent->client->print(F("=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.4\/js\/bootstrap.min.js\"><\/script><link rel=\"stylesheet\" href=\"http:\/\/bootswatch.com\/paper\/bootstrap.min.css\"><link href='http:\/\/fonts.googleapis.com\/css?family=Roboto' rel='stylesheet' type='text\/css'><script>var url=\"\";var bindingManager={data:{},liste"));
-			castedEvent->client->print(F("ners:{},activeRequests:0,addListener:function(f,b,c,a){var e=this.listeners[f];if(typeof e===\"undefined\"){e=[];this.listeners[f]=e}var d=function(){var g=bindingManager.data[f];c(b,g,a)};e.push(d);d()},set:function(d,c){if(this.data[d]!==c){this.data[d]=c;var b=this.listeners[d];if(typeof b!==\"undef"));
-			castedEvent->client->print(F("ined\"&&b!==null){for(var a=0;a<b.length;a++){b[a]()}}}}};var toggleClass=function(d,b,c){for(var e in c){var a=c[e];if(e!=b){d.parent().removeClass(a)}}if(c[b]){d.parent().addClass(c[b])}};var setText=function(c,a,b){if(typeof b!==\"undefined\"&&b[a]){a=b[a]}c.text(a)};var invoke=function(c,b,a){bindi"));
-			castedEvent->client->print(F("ngManager.activeRequests++;$.get(url+c,function(e){bindingManager.activeRequests--;for(var d in e){bindingManager.set(d,e[d])}if(b){b()}}).fail(function(){bindingManager.activeRequests--;if(a){a()}})};setInterval(function(){if(bindingManager.activeRequests>0){return}invoke(\"getState\")},1000);$(funct"));
-			castedEvent->client->print(F("ion(){$(\"[bindToProperty]\").each(function(){var b=$(this);var d=b.attr(\"bindToProperty\");var c=b.attr(\"textToggler\");if(c){c=JSON.parse(c)}bindingManager.addListener(d,b,setText,c);var a=b.attr(\"classToggler\");if(a){a=JSON.parse(a);bindingManager.addListener(d,b,toggleClass,a)}})});<\/script><style>."));
-			castedEvent->client->print(F("content{font-size:21px;font-weight:300;line-height:1.4}<\/style><\/head><body class=\"content\"><br\/><div class=\"container-fluid\"><div class=\"row\"><div class=\"col-md-3\"><div class=\"panel panel-primary text-center\"><div class=\"panel-heading\"><\/div><div class=\"panel-body\"><button class=\"btn btn-block btn-"));
-			castedEvent->client->print(F("lg\" type=\"button\" onclick=\"var cmd=window.prompt('Enter command name and push the desired button on the IR remote after pressing OK:'); if (!cmd) return; invoke('irLearnCommand?name='+cmd, function() { alert('Command saved'); }, function() { alert('Error saving command'); });\"> Learn IR command <\/bu"));
-			castedEvent->client->print(F("tton><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('acOff')\"> AC OFF <\/button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('acOn24')\"> AC ON 24C <\/button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('acFanHigh')\"> acFanHigh <\/button><\/"));
-			castedEvent->client->print(F("div><\/div><\/div><\/div><div class=\"row\"><div class=\"col-md-12\"><span class=\"label label-default\"><span class=\"glyphicon glyphicon-dashboard\"><\/span> Free memory (SRAM): <span bindToProperty=\"FREE_MEM\"><\/span> bytes<\/span><\/div><\/div><\/div><\/body>"));
->>>>>>> refs/remotes/origin/master
 			return;
 		} else if (strcmp_P(command, PSTR("getState")) == 0) {
 			urlFound = true;
@@ -116,11 +102,7 @@ public:
 
 		acOff.name = "acOff";
 
-<<<<<<< HEAD
-		iRLearner1.pin = 2;
-=======
 		iRLearner1.pin = A2;
->>>>>>> refs/remotes/origin/master
 
 		acOn24.name = "acOn24";
 
