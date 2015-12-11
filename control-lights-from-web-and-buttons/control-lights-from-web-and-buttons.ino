50aae2081d0e94dde5ab48f0538bd22c70ba4b43
Claudiu Matei <claudiu@shmessenger.org>
2015-12-11 14:36:06 +0200

    Meamodel updates for WiFi AP mode; Robot project

--- control-lights-from-web-and-buttons/control-lights-from-web-and-buttons.ino
+++ control-lights-from-web-and-buttons/control-lights-from-web-and-buttons.ino
@@ -2,6 +2,7 @@
 #include <FlowerPlatformArduinoRuntime.h>
 #include <SPI.h>
 #include <HttpServer.h>
+#include <EthernetNetworkAdapter.h>
 #include <Output.h>
 #include <Input.h>
 
@@ -12,6 +13,7 @@ protected:
 	Output output1;
 	Output output2;
 	HttpServer httpServer1;
+	EthernetNetworkAdapter ethernetNetworkAdapter1;
 
 	virtual void input1_onValueChanged(Event* event) {
 		ValueChangedEvent* castedEvent = (ValueChangedEvent*) event;
@@ -34,21 +36,24 @@ protected:
 		castedEvent->server->getCommandFromUrl(castedEvent->url, command);
 		bool urlFound = false;
 		
+		BufferedPrint out(castedEvent->client);
+		
 		if (strcmp_P(command, PSTR("")) == 0) {
 			urlFound = true;
-			castedEvent->server->httpSuccess(HttpServer::CONTENT_TYPE_HTML);
-			castedEvent->client->print(F("<!DOCTYPE html><html><head><meta charset=\"ISO-8859-1\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><script src=\"http:\/\/code.jquery.com\/jquery-1.11.2.min.js\"><\/script><link rel=\"stylesheet\" href=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.4\/css\/bootstrap.min.css\"><script src"));
-			castedEvent->client->print(F("=\"https:\/\/maxcdn.bootstrapcdn.com\/bootstrap\/3.3.4\/js\/bootstrap.min.js\"><\/script><link rel=\"stylesheet\" href=\"http:\/\/bootswatch.com\/paper\/bootstrap.min.css\"><link href='http:\/\/fonts.googleapis.com\/css?family=Roboto' rel='stylesheet' type='text\/css'><script>var url=\"\";var bindingManager={data:{},liste"));
-			castedEvent->client->print(F("ners:{},activeRequests:0,addListener:function(f,b,c,a){var e=this.listeners[f];if(typeof e===\"undefined\"){e=[];this.listeners[f]=e}var d=function(){var g=bindingManager.data[f];c(b,g,a)};e.push(d);d()},set:function(d,c){if(this.data[d]!==c){this.data[d]=c;var b=this.listeners[d];if(typeof b!==\"undef"));
-			castedEvent->client->print(F("ined\"&&b!==null){for(var a=0;a<b.length;a++){b[a]()}}}}};var toggleClass=function(d,b,c){for(var e in c){var a=c[e];if(e!=b){d.parent().removeClass(a)}}if(c[b]){d.parent().addClass(c[b])}};var setText=function(c,a,b){if(typeof b!==\"undefined\"&&b[a]){a=b[a]}c.text(a)};var invoke=function(c,b,a){bindi"));
-			castedEvent->client->print(F("ngManager.activeRequests++;$.get(url+c,function(e){bindingManager.activeRequests--;for(var d in e){bindingManager.set(d,e[d])}if(b){b()}}).fail(function(){bindingManager.activeRequests--;if(a){a()}})};setInterval(function(){if(bindingManager.activeRequests>0){return}invoke(\"getState\")},1000);$(funct"));
-			castedEvent->client->print(F("ion(){$(\"[bindToProperty]\").each(function(){var b=$(this);var d=b.attr(\"bindToProperty\");var c=b.attr(\"textToggler\");if(c){c=JSON.parse(c)}bindingManager.addListener(d,b,setText,c);var a=b.attr(\"classToggler\");if(a){a=JSON.parse(a);bindingManager.addListener(d,b,toggleClass,a)}})});<\/script><style>."));
-			castedEvent->client->print(F("content{font-size:21px;font-weight:300;line-height:1.4}<\/style><\/head><body class=\"content\"><br\/><div class=\"container-fluid\"><div class=\"row\"><div class=\"col-md-2\"><div class=\"panel panel-primary text-center\"><div class=\"panel-heading\">Light 1<\/div><div class=\"panel-body\"><div class=\"label\"> State:"));
-			castedEvent->client->print(F(" <span classToggler='{\"1\":\"label-warning\",\"0\":\"label-success\"}' bindToProperty=\"output1\" textToggler='{\"1\":\"OFF\",\"0\":\"ON\"}'><\/span><\/div><div> Commands: <\/div><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light1Toggle')\"> Toggle <\/button><button class=\"btn btn-block btn-lg\" typ"));
-			castedEvent->client->print(F("e=\"button\" onclick=\"invoke('light1On')\"> Turn On <\/button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light1Off')\"> Turn Off <\/button><\/div><\/div><\/div><div class=\"col-md-2\"><div class=\"panel panel-primary text-center\"><div class=\"panel-heading\">Light 2<\/div><div class=\"panel"));
-			castedEvent->client->print(F("-body\"><div class=\"label\"> State: <span classToggler='{\"1\":\"label-warning\",\"0\":\"label-success\"}' bindToProperty=\"output2\" textToggler='{\"1\":\"OFF\",\"0\":\"ON\"}'><\/span><\/div><div> Commands: <\/div><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light2Toggle')\"> Toggle <\/button><button"));
-			castedEvent->client->print(F(" class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light2On')\"> Turn On <\/button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light2Off')\"> Turn Off <\/button><\/div><\/div><\/div><\/div><div class=\"row\"><div class=\"col-md-12\"><span class=\"label label-default\"><span class"));
-			castedEvent->client->print(F("=\"glyphicon glyphicon-dashboard\"><\/span> Free memory (SRAM): <span bindToProperty=\"FREE_MEM\"><\/span> bytes<\/span><\/div><\/div><\/div><\/body>"));
+			castedEvent->server->httpSuccess(&out, HttpServer::CONTENT_TYPE_HTML);
+			out.write_P(PSTR("<!DOCTYPE html><html><head><meta charset=\"ISO-8859-1\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><script src=\"http://code.jquery.com/jquery-1.11.2.min.js\"></script><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css\"><script src"));
+			out.write_P(PSTR("=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js\"></script><link rel=\"stylesheet\" href=\"http://bootswatch.com/paper/bootstrap.min.css\"><link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'><script>var url=\"\";var bindingManager={data:{},liste"));
+			out.write_P(PSTR("ners:{},activeRequests:0,addListener:function(f,b,c,a){var e=this.listeners[f];if(typeof e===\"undefined\"){e=[];this.listeners[f]=e}var d=function(){var g=bindingManager.data[f];c(b,g,a)};e.push(d);d()},set:function(d,c){if(this.data[d]!==c){this.data[d]=c;var b=this.listeners[d];if(typeof b!==\"undef"));
+			out.write_P(PSTR("ined\"&&b!==null){for(var a=0;a<b.length;a++){b[a]()}}}}};var toggleClass=function(d,b,c){for(var e in c){var a=c[e];if(e!=b){d.parent().removeClass(a)}}if(c[b]){d.parent().addClass(c[b])}};var setText=function(c,a,b){if(typeof b!==\"undefined\"&&b[a]){a=b[a]}c.text(a)};var invoke=function(c,b,a){bindi"));
+			out.write_P(PSTR("ngManager.activeRequests++;$.ajax({url:url+c,success:function(e){bindingManager.activeRequests--;for(var d in e){bindingManager.set(d,e[d])}if(b){b()}},error:function(){bindingManager.activeRequests--;if(a){a()}},timeout:10000})};$(function(){$(\"[bindToProperty]\").each(function(){var b=$(this);var d"));
+			out.write_P(PSTR("=b.attr(\"bindToProperty\");var c=b.attr(\"textToggler\");if(c){c=JSON.parse(c)}bindingManager.addListener(d,b,setText,c);var a=b.attr(\"classToggler\");if(a){a=JSON.parse(a);bindingManager.addListener(d,b,toggleClass,a)}});setInterval(function(){if(bindingManager.activeRequests>0){return}invoke(\"getState"));
+			out.write_P(PSTR("\")},1000)});</script><style>.content{font-size:21px;font-weight:300;line-height:1.4}</style></head><body class=\"content\"><br/><div class=\"container-fluid\"><div class=\"row\"><div class=\"col-md-2\"><div class=\"panel panel-primary text-center\"><div class=\"panel-heading\">Light 1</div><div class=\"panel-bod"));
+			out.write_P(PSTR("y\"><div class=\"label\"> State: <span classToggler='{\"1\":\"label-warning\",\"0\":\"label-success\"}' bindToProperty=\"output1\" textToggler='{\"1\":\"OFF\",\"0\":\"ON\"}'></span></div><div> Commands: </div><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light1Toggle')\"> Toggle </button><button cla"));
+			out.write_P(PSTR("ss=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light1On')\"> Turn On </button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light1Off')\"> Turn Off </button></div></div></div><div class=\"col-md-2\"><div class=\"panel panel-primary text-center\"><div class=\"panel-heading\">L"));
+			out.write_P(PSTR("ight 2</div><div class=\"panel-body\"><div class=\"label\"> State: <span classToggler='{\"1\":\"label-warning\",\"0\":\"label-success\"}' bindToProperty=\"output2\" textToggler='{\"1\":\"OFF\",\"0\":\"ON\"}'></span></div><div> Commands: </div><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light2Toggl"));
+			out.write_P(PSTR("e')\"> Toggle </button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light2On')\"> Turn On </button><button class=\"btn btn-block btn-lg\" type=\"button\" onclick=\"invoke('light2Off')\"> Turn Off </button></div></div></div></div><div class=\"row\"><div class=\"col-md-12\"><span class=\"lab"));
+			out.write_P(PSTR("el label-default\"><span class=\"glyphicon glyphicon-dashboard\"></span> Free memory (SRAM): <span bindToProperty=\"FREE_MEM\"></span> bytes</span></div></div></div></body>"));
+			out.flush();
 			return;
 		} else if (strcmp_P(command, PSTR("getState")) == 0) {
 			urlFound = true;
@@ -85,11 +90,12 @@ protected:
 		}
 		
 		if (urlFound) {
-			castedEvent->server->httpSuccess();
-			printStateAsJson(castedEvent->client);
+			castedEvent->server->httpSuccess(&out);
+			printStateAsJson(&out);
 		} else {
-			castedEvent->server->httpError404();
+			castedEvent->server->httpError404(&out);
 		}
+		out.flush();
 	}
 	
 public:
@@ -102,27 +108,32 @@ public:
 		input1.pin = 8;
 		input1.internalPullUp = true;
 		input1.pollInterval = 200;
-		input1.setup();
 
 		input2.pin = 9;
 		input2.internalPullUp = true;
 		input2.pollInterval = 200;
-		input2.setup();
 
 		output1.pin = 2;
 		output1.initialValue = 1;
-		output1.setup();
 
 		output2.pin = 3;
 		output2.initialValue = 1;
-		output2.setup();
 
-		uint8_t macAddress[] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55 };
 		uint8_t ipAddress[] = { 192, 168, 100, 91 };
-		memcpy(httpServer1.macAddress, macAddress, 6);
 		memcpy(httpServer1.ipAddress, ipAddress, 4);
 		httpServer1.port = 80;
+
+		uint8_t macAddress[] = { 0x0E, 0xBB, 0xCC, 0xD5, 0xE2, 0xF4 };
+		memcpy(ethernetNetworkAdapter1.macAddress, macAddress, 6);
+		ethernetNetworkAdapter1.httpServer = &httpServer1;
+
+
+		input1.setup();
+		input2.setup();
+		output1.setup();
+		output2.setup();
 		httpServer1.setup();
+		ethernetNetworkAdapter1.setup();
 
 		input1.onValueChanged = new DelegatingListener<ApplicationGen>(this, &ApplicationGen::input1_onValueChanged);
 		input2.onValueChanged = new DelegatingListener<ApplicationGen>(this, &ApplicationGen::input2_onValueChanged);
@@ -132,7 +143,7 @@ public:
 	virtual void loop() {
 		input1.loop();
 		input2.loop();
-		httpServer1.loop();
+		ethernetNetworkAdapter1.loop();
 	}
 	
 	void printStateAsJson(Print* print) {
@@ -147,18 +158,15 @@ public:
 		output2.printStateAsJson(F("output2"), print);
 		print->print(F(","));
 
-		print->print(F("\"FREE_MEM\": "));
+		write_P(print, PSTR("\"FREE_MEM\": "));
 		print->print(freeRam());
-		print->print(F("}"));
+		print->print("}");
 	}
 
 };
 
 
-#define SHOULD_INCLUDE_CUSTOM_CODE
-#include "CustomCode.h"
-
-ApplicationGen* app = new ApplicationCustom();
+ApplicationGen* app = new ApplicationGen();
 
 void setup() {
 	app->setup();
